19e922c3f64b82d34746fa58b2ebf47d9f372220
Anushka <43216461+Anushkasandaruwankk@users.noreply.github.com>
2020-07-21 19:15:14 +0530

    commit

--- TestAutomation.java
+++ TestAutomation.java
@@ -6,3 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+public class TestAutomation {
+
+    
+}
